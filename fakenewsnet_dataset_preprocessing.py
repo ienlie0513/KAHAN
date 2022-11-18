6eaad74bb55bf28c8cbff05b472a5ff5c0176506
oysteinlondal <oystein@nenett.no>
2022-11-18 18:41:30 +0100

    verified and made changes so that modules are imported correctly

--- fakenewsnet_dataset_preprocessing.py
+++ fakenewsnet_dataset_preprocessing.py
@@ -3,3 +3,10 @@
 #       where id is the id of the news article, content is the content of the news article, comments is the Twitter comments of the news article, and label is the label of the news article (0 or 1).
 #       The TSV-files should be saved in the same directory as this script.
 #       The TSV-files should be named "politifact_no_ignore_s.tsv" and "gossipcop_no_ignore_s.tsv".
+
+import os
+
+# make a new directory for the preprocessed data
+dir_path = os.getcwd() + '/data'
+os.makedirs(dir_path, exist_ok=True)
+
